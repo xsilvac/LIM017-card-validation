009edb3acce5b7fa2cb9c657a8b9cdebb60c51bc
ximena silva <ximenasilvacotrina11@gmail.com>
2022-02-10 09:28:29 -0500

    funcionalidad del boton

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-import validator from './validator.js';
+//import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+document.getElementById("send").addEventListener("click", inputName);
+
+function inputName(){
+    let nameCard = document.getElementById("cardName").value;
+    alert("Bienvenida " + nameCard + " !!!");
+    document.getElementById("onePage").style.display = "none";
+    document.getElementById("twoPage").style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* //list to select month
+for (let i = 1; i <= 12; i++) {
+    let list = document.createElement('option');
+    list.value = i;
+    list.innerText = i;
+    selectMonth.appendChild(list);
+}
+//list to select year
+for (let i = 2010; i <= 2030; i++) {
+    let lists = document.createElement('option');
+    lists.value = i;
+    lists.innerText = i;
+    selectYear.appendChild(lists);
+} */
