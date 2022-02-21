dbefae741c8f2817dad7c5bc0dc7ab33e399bf35
ximena silva <ximenasilvacotrina11@gmail.com>
2022-02-21 10:47:27 -0500

    Modificaciones de fondo de pantalla y diseño

--- src/index.js
+++ src/index.js
@@ -1,78 +1,63 @@
-//import validator from './validator.js';
-
-//console.log(validator);
-
 import validator from './validator.js';
 
+// //list to select month
+// for (let i = 1; i <= 12; i++) {
+//     let list = document.createElement('option');
+//     list.value = i;
+//     list.innerText = i;
+//     selectMonth.appendChild(list);
+// }
 
+// //list to select year
+// for (let i = 2010; i <= 2030; i++) {
+//     let lists = document.createElement('option');
+//     lists.value = i;
+//     lists.innerText = i;
+//     selectYear.appendChild(lists);
+// }
 
 const btn = document.getElementById("send");
 btn.addEventListener("click", inputName);
 function inputName() {
-   let cardNumber = document.getElementById("cardNumber").value;
-//    console.log(typeof cardNumber);
-    const newAlert = document.getElementById("newAlert");
-    /* const newP = document.getElementById("newP"); */
+    const cardNumber = document.getElementById("cardNumber").value;
+    const correctAnswer = document.getElementById("correctAnswer");
+    const wrongAnswer = document.getElementById("wrongAnswer");
+    const showCard = document.getElementById("showCard");
+    const finalText = validator.isValid(cardNumber);
+    const maskedFinal = validator.maskify(cardNumber);
+
     if (cardNumber === "") {
-        newAlert.innerText = "Digite el número de su tarjeta";
-        newAlert.classList.add("errorText");
+        let newAlert = document.getElementById("newAlert");
+        newAlert.innerHTML = "Digite el número de su tarjeta, por favor";
     }
     else {
-        const finalText = validator.maskify(cardNumber);
-
-        return finalText;
+        document.getElementById("onePage").style.display = "none";
+        document.getElementById("twoPage").style.display = "block";
+        const cardName = document.getElementById("cardName").value;
+        const userName = document.getElementById("userName");
+        let imageAnswer;
+        if (finalText == true) {
+            userName.innerHTML = cardName;
+            showCard.innerHTML = maskedFinal;
+            imageAnswer = "check.png";
+            correctAnswer.innerHTML = "";
+            correctAnswer.innerHTML = `Es válida <br> Gracias por su preferencia.`;
+        }
+        else {
+            userName.innerHTML = cardName;
+            showCard.innerHTML = maskedFinal;
+            imageAnswer = "aspa.png";
+            wrongAnswer.innerHTML = "";
+            wrongAnswer.innerHTML = "Es inválida, intente nuevamente";
+        }
+        document.getElementById("imageAnswer").src = imageAnswer;
     }
-    
-    /* newP.innerText = finalText;
-     newP.classList.remove("errorText"); */
 
-    document.getElementById("onePage").style.display = "none";
-    document.getElementById("twoPage").style.display = "block";
-
+    return finalText;
 }
-
-
 
 const homePage = document.getElementById("homePage");
 homePage.addEventListener("click", () => {
     document.getElementById("twoPage").style.display = "none";
     document.getElementById("onePage").style.display = "block";
 })
-
-
-
-
-
-/* const btn = document.getElementById("send");
-btn.addEventListener("click", inputName);
-function inputName() {
-  let cardName = document.getElementById("cardName").value;
-    if (cardName == "") {
-        //alert("Ingresa tu nombre");
-    }
-    else {
-        let userName = document.getElementById("userName");
-        userName.innerHTML = cardName; */
-    //}
-
-
-
-
-
-
-
-
-/* //list to select month
-for (let i = 1; i <= 12; i++) {
-    let list = document.createElement('option');
-    list.value = i;
-    list.innerText = i;
-    selectMonth.appendChild(list);
-}
-//list to select year
-for (let i = 2010; i <= 2030; i++) {
-    let lists = document.createElement('option');
-    lists.value = i;
-    lists.innerText = i;
-    selectYear.appendChild(lists);
-} */
--- src/validator.js
+++ src/validator.js
@@ -1,16 +1,16 @@
 const validator = {
   isValid: function (str) {
-    let firstArray = Array.from(str);
-    let secondArray = [];
+    const firstArray = Array.from(str);
+    const secondArray = [];
     for (let i = 0; i < firstArray.length; i++) {
       secondArray.push(parseInt(firstArray[i]))
     }
-    let reverseArray = secondArray.reverse();
+    const reverseArray = secondArray.reverse();
     let sumOfEven = 0;
     let sumOfOdd = 0;
     for (let i = 1; i < reverseArray.length; i += 2) {
 
-      let valueOfTheEvenPosition = reverseArray[i] * 2;
+      const valueOfTheEvenPosition = reverseArray[i] * 2;
       if (valueOfTheEvenPosition >= 10) {
         sumOfEven = sumOfEven + 1 + valueOfTheEvenPosition % 10;
       }
@@ -19,7 +19,7 @@ const validator = {
     for (let i = 0; i < reverseArray.length; i += 2) {
       sumOfOdd = sumOfOdd + reverseArray[i];
     }
-    let finalSum = sumOfEven + sumOfOdd;
+    const finalSum = sumOfEven + sumOfOdd;
     let finalAnswer;
     if (finalSum % 10 == 0) {
       finalAnswer = true;
@@ -29,24 +29,22 @@ const validator = {
     return finalAnswer;
   },
 
-  maskify: function (x) {
+  maskify: function (mask) {
     //sacamos la cantidad de numeros de la tarjeta
-    let lengthOfCard = x.length;
+    const lengthOfCard = mask.length;
     //restamos 4 digitos a la cantidad de numeros de tarjeta
-    let firstDigits = lengthOfCard - 4
+    const firstDigits = lengthOfCard - 4
     //quitamos los cantidad de la tarjeta - los 4 ultimos digitos y nos quedamos con los 4 ultimos digitos
-    let separateDigits = x.slice(firstDigits, lengthOfCard);
+    const separateDigits = mask.slice(firstDigits, lengthOfCard);
     // console.log(separacion);
     let maskedDigits;
 
-    if (lengthOfCard > 5) {
+    if (lengthOfCard > 4) {
       maskedDigits = "#".repeat(firstDigits) + separateDigits;
     } else {
-      maskedDigits = x;
+      maskedDigits = mask;
     }
     return maskedDigits;
-
   }
-
 }
 export default validator;
